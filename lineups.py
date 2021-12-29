19c83019e3b20f7238d85f4a4b294d812b780166
SXB8VET <SCOTT_BERRY@homedepot.com>
2021-12-29 13:43:33 -0500

    lineups.py

--- lineups.py
+++ lineups.py
@@ -1,7 +1,7 @@
 import itertools
 
 
-def unique_lineups(subsets):
+def unique_subsets(subsets):
     cleaned_subsets = []
     for combination in subsets:
         if combination not in cleaned_subsets:
@@ -9,25 +9,24 @@ def unique_lineups(subsets):
     return cleaned_subsets
 
 
-def calc_lineups(players, num, max_total):
+def calc_lineups(players, max_total):
     subsets = []
     for L in range(0, len(players) + 1):
-        for subset in itertools.combinations(players, num):
+        for subset in itertools.combinations(players, 5):
             subset = sorted(list(subset), reverse=True)
             if sum(subset) == max_total:
                 subsets.append(subset)
-    cleaned_subsets = unique_lineups(subsets)
+    cleaned_subsets = unique_subsets(subsets)
     return cleaned_subsets
 
 
 def main():
     skill_levels = [3, 3, 4, 5, 5, 5, 5, 6]
-    num_players = 5
     max_skill_level_totals = range(23, 0, -1)
-    for skill_level in max_skill_level_totals:
-        lineups = calc_lineups(skill_levels, num_players, skill_level)
+    for skill_total in max_skill_level_totals:
+        lineups = calc_lineups(skill_levels, skill_total)
         if len(lineups) > 0:
-            print("Combinations of", skill_level)
+            print("Combinations of", skill_total)
             for lineup in lineups:
                 print(lineup)
 
