93f557a115304c19d0fd4c2cd4ed4d356f36b26e
berryscottr <berryscottr@gmail.com>
2022-03-02 23:52:33 -0500

    week 8 games career stats

--- predict.py
+++ predict.py
@@ -1,5 +1,9 @@
+import numpy as np
 import openpyxl
 import pandas as pd
+from matplotlib import pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def workbook2df(path, first_row_header, first_column_index):
@@ -15,11 +19,72 @@ def workbook2df(path, first_row_header, first_column_index):
     return df
 
 
+class SLMatches:
+    def __init__(self, SL, opponentSL):
+        self.SL = SL
+        self.opponentSL = opponentSL
+        self.points = 0
+        self.games = 0
+        self.indices_checked = []
+        self.average = None
+
+    def addpoints(self, points):
+        self.points += points
+
+    def addgame(self, index):
+        self.games += 1
+        self.indices_checked.append(index)
+
+    def getaverage(self):
+        try:
+            self.average = self.points / self.games
+        except ZeroDivisionError:
+            self.average = None
+
+
+def get_sl_matchup_stats(df, games2win):
+    slmatches = games2win
+    slrange = range(2, 8)
+    for p1skill in slrange:
+        for p2skill in slrange:
+            matchup_data = SLMatches(p1skill, p2skill)
+            for index, row in df.iterrows():
+                if row['Player_1'] == p1skill and row['Player_2'] == p2skill:
+                    matchup_data.addpoints(row['Points_1'])
+                    matchup_data.addgame(index)
+                    if p1skill == p2skill:
+                        matchup_data.addpoints(row['Points_2'])
+            for index, row in df.iterrows():
+                if index not in matchup_data.indices_checked:
+                    if row['Player_2'] == p1skill and row['Player_1'] == p2skill:
+                        matchup_data.addpoints(row['Points_2'])
+                        matchup_data.addgame(index)
+            matchup_data.getaverage()
+            if p1skill == p2skill:
+                matchup_data.average /= 2
+            try:
+                slmatches.loc[p1skill, p2skill] = round(matchup_data.average, 2)
+            except TypeError:
+                slmatches.loc[p1skill, p2skill] = matchup_data.average
+    # for sl in slrange:
+    #     plt.plot(np.array(slrange), np.array(slmatches.values[sl-2]))
+    #     title = 'Matchups SL {}'.format(sl)
+    #     imgtitle = "data/slmatchups/slmatchups{}.jpg".format(sl)
+    #     plt.title(title)
+    #     plt.xlabel('Opponent SL')
+    #     plt.ylabel('Average Points')
+    #     plt.savefig(imgtitle)
+    #     plt.show()
+    slmatches.to_excel(r'data/slmatchups/SLMatchupAverages.xlsx', index=True, header=True)
+    print(slmatches)
+
+
 def main():
     games_to_win = workbook2df("data/gamesToWin.xlsx", True, True)
-    player_data = workbook2df("data/wookieMistakesPlayerData.xlsx", True, False)
-    game_data = workbook2df("data/wookieMistakesSpring2022Games.xlsx", True, False)
-    pass
+    sl_matchup_data = workbook2df("data/slmatchups/SLMatchups.xlsx", True, False)
+    # player_data = workbook2df("data/wookieMistakesPlayerData.xlsx", True, False)
+    # game_data = workbook2df("data/wookieMistakesSpring2022Games.xlsx", True, False)
+    get_sl_matchup_stats(sl_matchup_data, games_to_win)
 
 
 if __name__ == '__main__':
